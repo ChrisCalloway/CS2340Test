011102a471ce509cf5059c46b8aff0f9a92cda1b
Marcia Schulman <marcia.schulman@outlook.com>
2017-03-22 12:54:00 -0400

    added layouts for water report fragment, water purity report fragment, get address activity

---
+++ Sustain/app/src/main/java/com/project/sustain/controllers/SubmitWaterReportFragment.java
@@ -0,0 +1,34 @@
+package com.project.sustain.controllers;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.project.sustain.R;
+import com.project.sustain.model.WaterReport;
+
+/**
+ * Created by Marcia on 3/22/2017.
+ */
+
+public class SubmitWaterReportFragment extends Fragment {
+    private WaterReport mWaterReport;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mWaterReport = new WaterReport();
+        //note: layout is inflated in a separate method, onCreateView.
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //set the layout xml for this fragment, but don't show it yet (false).
+        //view will be added and shown by the activity.
+        return inflater.inflate(R.layout.activity_get_address, container, false);
+    }
+}
